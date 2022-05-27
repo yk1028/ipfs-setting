adbb33309a092970872164623211609ef9779f9b
songwongeun <wongeunsong94@gmail.com>
2022-05-27 14:01:11 +0900

    feat: remove swarm gen from init.sh

--- init.sh
+++ init.sh
@@ -21,11 +21,3 @@ source ~/.profile
 sudo curl -L "https://github.com/docker/compose/releases/download/1.28.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-
-# set swarm.key
-git clone https://github.com/Kubuxu/go-ipfs-swarm-key-gen.git
-cd go-ipfs-swarm-key-gen
-go build ipfs-swarm-key-gen/main.go
-mkdir ~/data
-mkdir ~/data/ipfs_data
-./main > ~/data/ipfs_data/swarm.key
